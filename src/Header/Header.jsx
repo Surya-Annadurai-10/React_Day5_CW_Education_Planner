9030b5a34e8a24458335ba58bf9fa2cdf641df38
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-12-24 21:42:08 +0530

    updated

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 const Header = () =>{
    return (
     <>
-    <header>
+    <header className={styles.header}>
         <h1>Education Planner</h1>
     </header>
     </>
