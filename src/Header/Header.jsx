e0da3da0a0528241480c20c915014bde60df4528
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-12-24 07:49:44 +0530

    education planner created

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+
+const Header = () =>{
+   return (
+    <>
+    <header>
+        <h1>Education Planner</h1>
+    </header>
+    </>
+   )
+}
+
+export default Header;
---
+++ src/myComponents/myComponents.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+import MainContent from "../MainContent/MainContent";
+import styles from './myComponents.module.css'
+
+const MyComponents = () =>{
+return (
+    <>
+    <Header />
+    <MainContent />
+    </>
+)
+}
+
+export default MyComponents;
