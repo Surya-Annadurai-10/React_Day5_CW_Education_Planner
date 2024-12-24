9ba0c2fc69e443c1941416e5de676335e19f8959
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-12-24 23:45:39 +0530

    updated

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import styles from './Card.module.css';
 
 const  Card = (props) =>{
+   const [count , setCount] = useState(Number(props.hourNum))
+   const [isVisible , setIsVisible] = useState(false);
+
+   const onAdd = ()=>{
+      setCount(count+ 1);
+   }  
+
+   const onSub = ()=>{
+      count == 0 ? setIsVisible(true) : setCount(count - 1);
+   }  
+
+  
+  console.log(props.subjectName , props.id , props.hourNum)
      return (
         <>
-         <div>
-            <div className={styles.subjects}>{props.subjectName}</div>
-            <div className={styles.hours}>{props.HoursNum}</div>
+          {!isVisible ? <div className={styles.container_card} >
+            <div className={styles.subjects}><span className={styles.span}>Subject :</span> {props.subjectName}</div>
+            <div className={styles.hours}><span  className={styles.span}>Hours :</span>{count}hr </div>
             <div className={styles.buttons}>
-                <button>+</button>
-                <button>-</button>
+                <button  className={styles.add}  onClick={onAdd}> +</button>
+                <button className={styles.minus}  onClick={onSub}>-</button>
             </div>
-         </div>
+         </div> : ""}
         </>
      )   
 }
--- src/MainContent/MainContent.jsx
+++ src/MainContent/MainContent.jsx
@@ -4,8 +4,10 @@ import Card from "../Card/Card";
 
 
 const MainContent = () =>{
+  const [id , setId] = useState(1);
     const [subject , setSubject] = useState("");
     const [hours , setHours] = useState("");
+    const [arr , setArr] = useState(JSON.parse(localStorage.getItem("subjectData")) == null ? [] : JSON.parse(localStorage.getItem("subjectData"))   );
    
    const onSubject = (e) =>{
     setSubject(e.target.value);
@@ -15,33 +17,61 @@ const MainContent = () =>{
     
    }
 
+   console.log(arr);
+   if(localStorage.getItem("data") == null){
+    let data = localStorage.setItem("subjectData" , JSON.stringify(arr));
+     
+   }else{
+     let data = JSON.parse(localStorage.getItem("subjectData"));
+     data.push({
+       id : id,
+       subjectName : subject,
+       hourNum : hours
+      
+   });
+   localStorage.setItem("subjectData" , data);
+   }
+
+
+
    const onBtnClick = () =>{
-        arr.push ({
-            subjectName : subject.toLowerCase(),
-            hoursNum : hours
-        });
-        
-        console.log(arr);
+    
+
+       setArr([...arr , {
+        id : id,
+        subjectName : subject,
+        hourNum : hours
+       
+    }]);
+        setId(id + 1);
         setHours("");
         setSubject("");
+      
     
    }
 
     return (
         <>
-         <div className={styles.container}>
+        <div className={styles.main_container}>
+        <div className={styles.container}>
           <div className={styles.input_container}>
-            <input  value={subject} onChange={onSubject} className={styles.subject_In} type="text" />
-            <input value={hours} onChange={onHour} className={styles.hours_In} type="number" />
+            <input placeholder={"Enter Subject"}  value={subject} onChange={onSubject} className={styles.subject_In} type="text" />
+            <input  placeholder={"Enter Hour"}  value={hours} onChange={onHour} className={styles.hours_In} type="number" />
             <div className={styles.button_section}>
                 <div className={styles.inner}></div>
                 <button onClick={onBtnClick}>Add</button>
             </div>
           </div>
-          <div>
-            <Card subjectName ={ subject} HoursNum = {hours} />
-          </div>
+        
          </div>
+         <div className={styles.card_container}>
+            {
+              arr.map((val) =>{
+                return  <Card key= {val.id}  {...val} />
+              })
+            }
+          </div>
+        </div>
         </>
     )
 }
